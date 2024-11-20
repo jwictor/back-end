80d37c41daa483762a0a4bd230bb25dd0706eb99
Joao Wictor <joaowictoroliveira3301@gmail.com>
2024-11-20 17:42:23 -0300

    feat: conexao ao banco

--- server.js
+++ server.js
@@ -1,52 +1,11 @@
 import express from 'express';
-
-const posts = [
-    {
-        id: 1,
-        descricao: "Uma foto teste",
-        imagem: "https://placecats.com/millie/300/150",
-    },
-    {
-        id: 2,
-        descricao: "Paisagem deslumbrante",
-        imagem: "https://placecats.com/millie/300/150"
-      },
-      {
-        id: 3,
-        descricao: "Cachorro fofo",
-        imagem: "https://placecats.com/millie/300/150"
-      },
-      {
-        id: 4,
-        descricao: "Comida deliciosa",
-        imagem: "https://placecats.com/millie/300/150"
-      },
-      {
-        id: 5,
-        descricao: "Citação inspiradora",
-        imagem: "https://placecats.com/millie/300/150"
-      }
-]
+import routes from './src/routes/postsRoutes.js';
 
 const app = express();
-app.use(express.json());
+routes(app);
 
 app.listen(3000, () => {
     console.log("Servidor escutando...");
 });
 
 
-app.get("/posts", (req, res) => {
-    res.status(200).json(posts);
-});
-
-function buscarPostID(id){
-    return posts.findIndex((post) => {
-        return post.id === Number(id)
-    });
-};
-
-app.get("/posts/:id", (req, res) => {
-    const index = buscarPostID(req.params.id);
-    res.status(200).json(posts[index]);
-});
